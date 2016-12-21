4d52b3fa5182488b770b3843d9f8de48e84864fb
Zorin Ivan <zorin.ivan@outlook.com>
2016-12-21 10:18:43 +0300

    Lesson 21.12.2106

---
+++ Example1/Example2/LongRunningProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    public class LongRunningProcess
+    {
+        public event EventHandler<DateTime> ProcessFinished;
+        //public event EventHandler ProcessFinished;
+
+        public void Run()
+        {
+            Thread.Sleep(5000);
+            if (ProcessFinished != null)
+                ProcessFinished(this, DateTime.Now);
+            //ProcessFinished(this, EventArgs.Empty);
+            Thread.Sleep(5000);
+        }
+
+        public static int GetProccesId()
+        { return 0; }
+    }
+}
--- Example1/Example2/Program.cs
+++ Example1/Example2/Program.cs
@@ -8,8 +8,42 @@ namespace Example2
 {
     class Program
     {
+        //DON'T USE STATIC FIELDS!!!
+        static LongRunningProcess _process1;
+
         static void Main(string[] args)
         {
+            Int32 i = 1;
+            i += 4;
+            i = i + 4;
+
+            _process1 = new LongRunningProcess();
+            _process1.ProcessFinished += OnProcessFinished;
+            LongRunningProcess p2 = new LongRunningProcess();
+            p2.ProcessFinished += OnProcessFinished;
+            _process1.Run();
+
+            var x = Math.Max(3, 4);
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
         }
+
+        static void Method2()
+        {
+            _process1.Run();
+            
+        }
+
+        public static void OnProcessFinished(Object sender,
+            DateTime e)
+        {
+            Console.WriteLine(e);
+        }
+
+        //public static void OnProcessFinished(Object sender,
+        //    EventArgs e)
+        //{
+        //    Console.WriteLine(DateTime.Now);
+        //}
     }
 }
